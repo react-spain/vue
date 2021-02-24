d53868250f2ec002ab66db5f59a8ee2f1f3f2bb2
Marlom <falconsoft.3d@gmail.com>
2021-02-24 16:47:56 +0100

    sc

---
+++ cli/hola-mundo-v2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Titulo texto="Mi banco dimnamico" />
+  <Cuenta/>
+</template>
+
+<script>
+import Titulo from './components/Titulo.vue'
+import Cuenta from './components/Cuenta.vue'
+export default {
+  name: 'App',
+  components: {
+        Titulo,
+        Cuenta
+      }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli/hola-mundo-v2/src/components/AccionSaldo.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="accion" :disabled="desactivar">{{  texto  }}</button>
+</template>
+
+<script>
+export default {
+  name: 'AccionSaldo',
+  props: {
+    texto: String,
+    desactivar: {
+        type: Boolean,
+        default: false
+    }
+  },
+  methods: {
+      accion(){
+        this.$emit('accion')
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
